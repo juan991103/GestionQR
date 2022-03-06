c646970cbd1e1ae29d2965ea884e6b6d59eb9335
jgilb <jgilb@JUANPEREZ-PC>
2022-03-06 18:56:23 -0400

    commit

---
+++ GestionQR/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SistemaRH.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            //write logic here  
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            //write logic here  
+            return View();
+        }
+    }
+}
